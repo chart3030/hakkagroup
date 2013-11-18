b328ff8d65ebac3d122fce3f6cf65245f30a8ceb
pete@g33ktalk.com <pete@mechanikal.com>
2013-11-18 11:04:56 -0800

    PHPMailer moved to main script

--- contactFormSubmit.php
+++ contactFormSubmit.php
@@ -2,9 +2,9 @@
 
 //require_once 'Mandrill.php'; //Not required with Composer
 //$mandrill = new Mandrill('i5dEhc969G4aqgT2tH6k0g');
-  
-require_once "class.phpmailer.php"
-require_once "class.smtp.php"
+
+require("PHPMailer-master/class.phpmailer.php");
+require("PHPMailer-master/class.smtp.php");
 
 // check for form submission - if it doesn't exist then send back to contact form  
 if (!isset($_POST["save"]) || $_POST["save"] != "contact") {  
@@ -42,6 +42,22 @@ if (isset($error)) {
     //header("Location: index.php?e=".urlencode($error)); exit;
     echo $error;  
 }  
+
+
+    $mail = new PHPMailer();
+    $mail->IsSMTP();  // telling the class to use SMTP
+    $mail->Host     = "smtp.gmail.com"; // SMTP server
+    $mail->From     = "suneetha.itham@gmail.com";
+    $mail->AddAddress("suneetha@yantranet.com");
+    $mail->Subject  = "First PHPMailer Message";
+    $mail->Body     = "Hi! \n\n This is my first e-mail sent through PHPMailer.";
+    $mail->WordWrap = 50;
+    if(!$mail->Send()) {
+        echo 'Message was not sent.';
+        echo 'Mailer error: ' . $mail->ErrorInfo;
+    }else {
+        echo 'Message has been sent.';
+    }
 
 /*          
 // write the email content  
